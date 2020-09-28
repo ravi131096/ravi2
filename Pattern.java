48c2f00768943ade9c11a72ab1b1e92c613df873
ravi131096 <rvrj.666@gmail.com>
2020-09-28 11:42:17 +0530

    raj

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+class Pattern
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int k=5;k>=i;k--)
+			{
+                System.out.print("*");
+			}
+	      System.out.println();
+		}
+	}
+}
